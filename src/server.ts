9dc0a216d23ac3af36629a6656298e3124698027
Douglas Ribeiro <douglasribeiroo2016@gmail.com>
2026-04-22 02:20:16 -0300

    configurações iniciais do prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from 'express'
 
-const port = 3000;
-const app = express();
+const port = 3000
+const app = express()
 
 app.get('/movies', (req, res) => {
-  res.send('Listagem de filmes');
-});
+    res.send('Listagem de filmes')
+})
 
 app.listen(port, () => {
-  console.log(`Servidor em execução na porta http://localhost:${port}`);
-});
+    console.log(`Servidor em execução na porta http://localhost:${port}`)
+})
