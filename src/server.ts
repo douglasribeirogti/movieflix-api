bc8b2f7016c071c1c66f081c1f4ea3ad8018a041
Douglas Ribeiro <douglasribeiroo2016@gmail.com>
2026-04-27 01:14:14 -0300

    adicionar atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -8,49 +8,83 @@ const prisma = new PrismaClient()
 app.use(express.json())
 
 app.get('/movies', async (_, res) => {
-    const movies = await prisma.movie.findMany({
-        orderBy: {
-            title: 'asc',
-        },
-        include: {
-            genres: true,
-            languages: true,
-        },
-    })
-    res.json(movies)
+  const movies = await prisma.movie.findMany({
+    orderBy: {
+      title: 'asc',
+    },
+    include: {
+      genres: true,
+      languages: true,
+    },
+  })
+  res.json(movies)
 })
 
 app.post('/movies', async (req, res) => {
-    const { title, genre_id, language_id, oscar_count, release_date } = req.body
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
-    try {
-        const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: { equals: title, mode: 'insensitive' } },
-        })
+  try {
+    const movieWithSameTitle = await prisma.movie.findFirst({
+      where: { title: { equals: title, mode: 'insensitive' } },
+    })
 
-        if (movieWithSameTitle) {
-            return res.status(409).send({
-                message: 'Já existe um filme cadastrado com esse título',
-            })
-        }
-
-        await prisma.movie.create({
-            data: {
-                title,
-                genre_id,
-                language_id,
-                oscar_count,
-                release_date: new Date(release_date),
-            },
-        })
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-        return res.status(500).send({ message: 'Falha ao cadastrar um filme' })
+    if (movieWithSameTitle) {
+      return res.status(409).send({
+        message: 'Já existe um filme cadastrado com esse título',
+      })
     }
 
-    res.status(201).send()
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return res.status(500).send({ message: 'Falha ao cadastrar um filme' })
+  }
+
+  res.status(201).send()
+})
+
+app.put('/movies/:id', async (req, res) => {
+  // pegar o id do registro que vai ser atualizado
+  const id = Number(req.params.id)
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!movie) {
+      return res.status(404).send({ message: 'Filme não encontrado' })
+    }
+
+    const data = { ...req.body }
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined
+
+    // pegar os dados do filme que será atualizado e atualizar ele no prisma.
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data: data,
+    })
+  } catch (error) {
+    return res.status(500).send({ message: 'Falha ao atualizar um filme' })
+  }
+  // retornar o status correto informando que o filme foi atualizado.
+  res.status(200).send()
 })
 
 app.listen(port, () => {
-    console.log(`Servidor em execução na porta http://localhost:${port}`)
+  console.log(`Servidor em execução na porta http://localhost:${port}`)
 })
