b53356a56a7149924edc7e6187cfb2eb1e146269
Douglas Ribeiro <douglasribeiroo2016@gmail.com>
2026-04-27 22:05:15 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -85,6 +85,24 @@ app.put('/movies/:id', async (req, res) => {
   res.status(200).send()
 })
 
+app.delete('/movies/:id', async (req, res) => {
+  const id = Number(req.params.id)
+
+  try {
+    const movie = await prisma.movie.findUnique({ where: { id } })
+
+    if (!movie) {
+      return res.status(404).send({ message: 'Filme não encontrado' })
+    }
+
+    await prisma.movie.delete({ where: { id } })
+
+    res.status(204).send()
+  } catch (error) {
+    return res.status(500).send({ message: 'Falha ao excluir o filme' })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Servidor em execução na porta http://localhost:${port}`)
 })
